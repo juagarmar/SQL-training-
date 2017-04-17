a1580301304ee8a49f62c1072d20744511e755b3
Juan Garcia <juagarmar4@gmail.com>
2017-04-17 09:13:48 +0100

    Create Table and Data.sql

---
+++ SQL easy app/Table and Data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE EMP (
+Empno INT PRIMARY KEY,
+Ename varchar(255) NOT NULL,
+Job varchar(255),
+Mgr INT,
+Hiderdate date,
+Sal INT,
+Comm INT,
+deptno INT 
+FOREIGN KEY (deptno) REFERENCES EMP (deptno));
